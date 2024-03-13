55a457e28af2c1f70d060c0f94c191353a686076
xdchfddfdd <161622667+xdchfddfdd@users.noreply.github.com>
2024-03-13 15:34:25 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    for (let i=0; i=100; i++){
+      console.log("Hello world!")
+    }
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
